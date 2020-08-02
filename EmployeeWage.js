5d3e4fd41a5e451ab6968dd1309253a55e844905
PrajyotGajane <prajyotgajane@gmail.com>
2020-08-02 20:59:21 +0530

    Added: Logic to calculate wage for part time and full time employee

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,11 +1,14 @@
 let EMP_RATE_PER_HOUR = 20;
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
 
-let isPresent = 1
+let empCheck = Math.floor(Math.random() * 10) % 3;
 let empHrs = 0, empWage = 0;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == isPresent)
+if (empCheck == IS_PART_TIME)
+    empHrs = 4;
+else if (empCheck == IS_FULL_TIME)
     empHrs = 8;
 else
     empHrs = 0;
 empWage = empHrs * EMP_RATE_PER_HOUR;
-console.log("Employee Wage : " + empWage);
+console.log("Employee Wage = "+empWage);
