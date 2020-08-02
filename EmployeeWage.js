04f89270993a2998f4a504febea46c103b40e112
PrajyotGajane <prajyotgajane@gmail.com>
2020-08-02 21:37:32 +0530

    Refactored: Used function to get working hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,25 +4,29 @@ let IS_FULL_TIME = 2;
 let NUM_OF_WORKING_DAYS = 20;
 let MAX_HRS_IN_MONTH = 100;
 
-let isPresent = 1;
 let monthlyWage = 0;
 let empHrs = 0, empWage = 0;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 
+function getWorkingHours(empCheck){
+    switch(empCheck) {
+        case IS_PART_TIME:
+            empHrs = 4;
+            break
+        case IS_FULL_TIME:
+            empHrs = 8;
+            break
+        default:
+            empHrs = 0;
+        }
+    return empHrs;
+}
+
 while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    switch(empCheck) {
-    case IS_PART_TIME:
-        empHrs = 4;
-        break
-    case IS_FULL_TIME:
-        empHrs = 8;
-        break
-    default:
-        empHrs = 0;
-    }
+    empHrs = getWorkingHours(empCheck);
     totalEmpHrs += empHrs;
 }
 
